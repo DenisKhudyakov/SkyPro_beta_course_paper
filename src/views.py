9b13a2c8684691c47db4e99511eb578711664e6d
Denis Khudyakov <denis.hudyakov27@gamil.com>
2023-12-01 22:29:39 +0500

    add func currence_rares, get_stock_data

--- src/views.py
+++ src/views.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 from pathlib import Path
 from data.config import PATH_XLS_FILE_WITH_OPERATION
@@ -6,9 +8,8 @@ from datetime import datetime
 import requests
 import re
 import json
+from dotenv import load_dotenv, find_dotenv
 
-
-API_KEY = 'f069fd48b23c4ff6819b137bb51c9c94'
 
 def read_xls_file(any_path: Path) -> dict:
     """
@@ -22,16 +23,49 @@ def read_xls_file(any_path: Path) -> dict:
 
 
 
-def get_api():
-    response = requests.get('https://ru.tradingview.com/symbols/SPX/components/')
-    text = re.findall(r'"response_json":.+</script></div>', response.text)[0][:-15]
-    print(response.text)
-    new_text = re.findall(r'\{(?:.*?)\}\]\}', text)
-    return json.loads(*new_text)
-
-def filter_stock_data(_dict: dict) -> list:
+def get_stock_data():
+    """Функция, которая получает стоимость акции, по названию акции"""
     stock_exchange_shares = ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]
-    return list(filter(lambda x: x['d'][0] in stock_exchange_shares, _dict['data']))
+    if not find_dotenv():
+        exit('Переменные окружения не загружены, т.к. отсутствует файл .env')
+    else:
+        load_dotenv()
+        stocks_info = []
+    API_KEY = os.getenv('API_KEY')
+    try:
+        for stock in stock_exchange_shares:
+            url = f"https://finnhub.io/api/v1/quote?symbol={stock}&token={API_KEY}"
+            response = requests.get(url)
+            response.raise_for_status()
+            response_data = response.json()
+            stock_price = response_data["c"]
+            info = {"stock": stock,
+                    "price": stock_price
+            }
+            stocks_info.append(info)
+        return stocks_info
+    except (requests.exceptions.HTTPError, ValueError, KeyError) as e:
+            print(e)
 
-get_api()
+def currency_rates(currencies: list) -> dict:
+    """
+    Функция отправляет запрос на сайт ЦБ РФ и получает курс валют в формате JSON
+    :param url: строка с адресом
+    :return: ничего не возвращаем
+    """
+    if not find_dotenv():
+        exit('Переменные окружения не загружены, т.к. отсутствует файл .env')
+    else:
+        load_dotenv()
+        url_api = os.getenv("URL")
+    if url_api is not None:
+        response = requests.get(url_api)
+        data_dict = response.json()
+        rates_info = []
+        for currency in currencies:
+            cbr_data = data_dict["Valute"][currency]["Value"]
+            info = {"currency": currency, "rate": round(cbr_data, 2)}
+            rates_info.append(info)
+        return rates_info
+
 
